1ff817b200d5bbc35e3103b65ad2304d235964e1
LuisZ <lzamudio@edgematrix.com>
2024-12-22 14:45:21 +0900

    fix: syntax

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '*',
+      path: '/*',
+      name: 'not-found',
       component: NotFoundView,
     },
     {
